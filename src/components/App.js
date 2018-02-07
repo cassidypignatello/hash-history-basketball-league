13445ba56ce60c34494611d8e78990aa83146e1e
cassidypignatello <cassidy.pignatello@gmail.com>
2018-02-06 19:07:21 -0500

    adds 404 route

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,30 @@
 import React, { Component } from 'react'
 import {
   BrowserRouter as Router,
-  Route
+  Route,
+  Switch
 } from 'react-router-dom'
 import Home from './Home'
 import Players from './Players'
 import Teams from './Teams'
 import Navbar from './Navbar'
 
-class App extends Component {
+export default class App extends Component {
   render() {
     return (
       <Router>
         <div>
           <Navbar />
-          <Route exact path='/' component={Home} />
-          <Route path='/players' component={Players} />
-          <Route path='/teams' component={Teams} />
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route path='/players' component={Players} />
+            <Route path='/teams' component={Teams} />
+            <Route render={({ location }) => (
+              <h3 className='text-center'>Sorry, {location.pathname} wasn't found. Please check the URL and try again.</h3>
+            )} />
+          </Switch>
         </div>
       </Router>
     )
   }
 }
-
-export default App
