ca8f36acec196bcf9871b3d7cf250f2a004859e8
cassidypignatello <cassidy.pignatello@gmail.com>
2018-02-10 18:45:31 -0500

    adds Sidebar and fetches players

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,10 +1,42 @@
 import React, { Component } from 'react'
+import { Route, Link } from 'react-router-dom'
+import Sidebar from './Sidebar'
+import { getPlayers } from '../api'
+import { parse } from 'query-string'
+import slug from 'slug'
 
 export default class Players extends Component {
+  state = {
+    loading: true,
+    players: []
+  }
+  componentDidMount() {
+    const { location } = this.props
+    location.search ?
+    this.fetchPlayers(parse(location.search).teamId) :
+    this.fetchPlayers()
+  }
+  fetchPlayers = (teamId) => {
+    getPlayers(teamId)
+      .then((players) => this.setState(() => ({
+        loading: false,
+        players,
+      })))
+  }
   render() {
-    return(
+    const { players, loading } = this.state
+    const { match, location } = this.props
+    return (
       <div className='container two-column'>
-        Players
+        <Sidebar
+          title='Players'
+          list={players.map((player) => player.name)}
+          loading={loading}
+          {...this.props}
+        />
+        {loading === false && location.pathname === '/players' ?
+          <div className='sidebar-instruction'>Select a Player</div> :
+          null}
       </div>
     )
   }
